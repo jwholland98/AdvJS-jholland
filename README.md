832d0c8b26c07e0395c2c70a2a4d8dfa3fb14dba
jwholland98 <holland.jesse55@gmail.com>
2020-01-29 11:38:39 -0700

    Finished kattis problem and updated README

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Spring 2020
 Jesse Holland
 ## Project Status
+
+###Kattis
+- Solved "What does the fox say?" (2.0 difficulty)
--- kattis/whatdoesthefoxsay.js
+++ kattis/whatdoesthefoxsay.js
@@ -1,47 +1,64 @@
 const readline = require('readline');
 const assert = require('assert').strict;
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 function answer(str, noises){//remove noises from str
-    return str[0];//just a test to make sure it is working so far
+    let count=0;
+    for(i=0;i<noises.length;i++){
+       for(j=0; j<str.length; j++){
+            if(str[j] === noises[i]){
+                str.splice(j, 1);
+                j-=1;
+            }
+        }
+    }
+    str = str.join(' ');
+    return str;
 }
 
 function test(){
-
+    assert.strictEqual(answer(["wa", "yay", "wa", "what", "wa"], ["yay", "what"]), "wa wa wa");
+    assert.strictEqual(answer(["wa", "yay", "yay", "yay", "wa", "what", "what", "what", "wa"], ["yay", "what"]), "wa wa wa");
+    assert.strictEqual(answer(["wa", "yay", "aw", "yay", "yay", "wa", "what", "what", "what", "wa", "pow", "pow"], ["yay", "what"]), "wa aw wa wa pow pow");
+    console.log("all test cases passed");
+    rl.close();
 }
 
 function solve() {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    let n = 0;
+    let lineCount = 1;
+    let str = [];
+    let noises = [];
+    let caseNum = 0;
     rl.on('line', (line) => {
-        n = line.split(' ');
-        rl.close();
-    })
-
-    for(i=0;i<n;i+=0){
-        let lineCount = 1;
-        let str = [];
-        let noises = [];
-        rl.on('line', (line) => {
-            if (lineCount == 1){
-                str = line.split(' ');
-                lineCount += 1;
-            }
+        if(lineCount == 1){
+            caseNum=line;
+            lineCount+=1;
+        }
+        else if (lineCount == 2){
+            str = line.split(' ');
+            lineCount += 1;
+        }
+        else{
+            let inp = line.split(' ');
+            if(inp[0] != "what")
+                noises.push(inp[2]);
             else{
-                //get remaining input and then call answer
-                let inp = line.split(' ');
-                if(inp[0] != "what")
-                    noises.push(inp[2]);
-                else{
-                    console.log(answer(str, noises));
-                    rl.close();
+                console.log(answer(str, noises));
+                if(caseNum > 1){
+                    caseNum-=1;
+                    lineCount=2;
+                    str=[];
+                    noises=[]
                 }
+                else
+                    rl.close();
             }
-        })
-    }
+        }
+    })
 }
 
 if (require.main == module) {
